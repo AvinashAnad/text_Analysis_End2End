0033adbce8f6aff1ba22ce3f083364f343fc5b1f
Greenville <avinashanad@gmail.com>
2019-07-16 23:01:08 +0530

    Updated the search parameter for clean and analyse

--- main_file.py
+++ main_file.py
@@ -1,12 +1,11 @@
 import extraction as ext
+import ca
 import ppt
 import email
 
 searchstring = str(input("Enter the Search search\n>>>"))
 datelimit = str(input("search for current date - (Enter Days)"))
 ext.extractfn(searchstring,datelimit)
-
-import ca
-ca.cleanNanalyse('')
+ca.cleanNanalyse(searchstring)
 ppt.ppt()
 email.sendemail()
